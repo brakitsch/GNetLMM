a8a0b0172db5ff2d125db634fb6cf8af4dcf1f1f
Barbara Rakitsch <rakitsch@ebi-004.ebi.ac.uk>
2015-12-01 20:57:13 +0000

    minors

--- demos/runGNetLMM.sh
+++ demos/runGNetLMM.sh
@@ -14,40 +14,39 @@ WINDOW=2000
 PLOTFILE=./out/power.pdf
 
 # Generate phenotypes
-#./../GNetLMM/bin/gNetLMM_simPheno --bfile $BFILE --pfile $PFILE
+./../GNetLMM/bin/gNetLMM_simPheno --bfile $BFILE --pfile $PFILE
 
 # Compute covariance matrix
-#./../GNetLMM/bin/gNetLMM_preprocess --compute_covariance --bfile $BFILE --cfile $CFILE 
+./../GNetLMM/bin/gNetLMM_preprocess --compute_covariance --bfile $BFILE --cfile $CFILE 
 
 # Run initial association scan
-#for i in $(seq 0 10000 40000)
-#do
-#    ./../GNetLMM/bin/gNetLMM_preprocess --initial_scan --bfile $BFILE --pfile $PFILE --cfile $CFILE.cov --assoc0file $ASSOC0FILE.startSnp_$i --startSnpIdx $i --nSnps 10000 
-#done
+for i in $(seq 0 10000 40000)
+do
+    ./../GNetLMM/bin/gNetLMM_preprocess --initial_scan --bfile $BFILE --pfile $PFILE --cfile $CFILE.cov --assoc0file $ASSOC0FILE.startSnp_$i --startSnpIdx $i --nSnps 10000 
+done
 
 # Merging results
-#./../GNetLMM/bin/gNetLMM_preprocess --merge_assoc0_scan  --assoc0file $ASSOC0FILE 
+./../GNetLMM/bin/gNetLMM_preprocess --merge_assoc0_scan  --assoc0file $ASSOC0FILE 
 
 # Compute marginal gene-gene correlations
-#./../GNetLMM/bin/gNetLMM_preprocess --gene_corr --pfile $PFILE --gfile $GFILE --bfile $BFILE
+./../GNetLMM/bin/gNetLMM_preprocess --gene_corr --pfile $PFILE --gfile $GFILE --bfile $BFILE
 
 # Precompute cis-anchors
-#./../GNetLMM/bin/gNetLMM_preprocess --cis_anchor  --bfile $BFILE --pfile $PFILE --assoc0file $ASSOC0FILE --cis_thres=$CIS_THRESH --cis_window=$CIS_WINDOW --assoc0file $ASSOC0FILE --cisfile $CISFILE
+./../GNetLMM/bin/gNetLMM_preprocess --cis_anchor  --bfile $BFILE --pfile $PFILE --assoc0file $ASSOC0FILE --cis_thres=$CIS_THRESH --cis_window=$CIS_WINDOW --assoc0file $ASSOC0FILE --cisfile $CISFILE
 
-#for i in $(seq 0 10 90)
-#do
-#    ./../GNetLMM/bin/gNetLMM_analyse --find_vstructures  --pfile $PFILE  --gfile $GFILE --cisfile $CISFILE --assoc0file $ASSOC0FILE --window $WINDOW --vfile $VFILE.startTrait_$i --bfile $BFILE --startTraitIdx $i --nTraits 10
+for i in $(seq 0 10 90)
+do
+    ./../GNetLMM/bin/gNetLMM_analyse --find_vstructures  --pfile $PFILE  --gfile $GFILE --cisfile $CISFILE --assoc0file $ASSOC0FILE --window $WINDOW --vfile $VFILE.startTrait_$i --bfile $BFILE --startTraitIdx $i --nTraits 10
     
-#     ./../GNetLMM/bin/gNetLMM_analyse --update_assoc --bfile $BFILE --pfile $PFILE --cfile $CFILE.cov  --vfile $VFILE.startTrait_$i --assocfile $ASSOCFILE.startTrait_$i --startTraitIdx $i --nTraits 10
-
-#done
+     ./../GNetLMM/bin/gNetLMM_analyse --update_assoc --bfile $BFILE --pfile $PFILE --cfile $CFILE.cov  --vfile $VFILE.startTrait_$i --assocfile $ASSOCFILE.startTrait_$i --startTraitIdx $i --nTraits 10
+done
 
 # Merge csv files
-#./../GNetLMM/bin/gNetLMM_analyse --concatenate --files $VFILE
-#./../GNetLMM/bin/gNetLMM_analyse --concatenate --files $ASSOCFILE
+./../GNetLMM/bin/gNetLMM_analyse --concatenate --files $VFILE
+./../GNetLMM/bin/gNetLMM_analyse --concatenate --files $ASSOCFILE
 
 # Write to matrix
-#./../GNetLMM/bin/gNetLMM_analyse --merge_assoc --assoc0file $ASSOC0FILE --assocfile $ASSOCFILE
+./../GNetLMM/bin/gNetLMM_analyse --merge_assoc --assoc0file $ASSOC0FILE --assocfile $ASSOCFILE
 
 # Plot results
 ./../GNetLMM/bin/gNetLMM_postprocess --assocfile $ASSOCFILE --assoc0file $ASSOC0FILE --plotfile $PLOTFILE --pfile $PFILE --bfile $BFILE --window $WINDOW
